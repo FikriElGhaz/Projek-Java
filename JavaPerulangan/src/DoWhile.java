88865533ad62e439935a9c815e554ae874cf9924
FikriElGhaz <49526898+FikriElGhaz@users.noreply.github.com>
2019-04-11 21:46:06 +0700

    Add files via upload

---
+++ JavaPerulangan/src/DoWhile.java
@@ -0,0 +1,10 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int x = 1;
+
+        do {
+            System.out.println("NOMER : " + x );
+            x++;
+        } while( x <= 20 );
+    }
+}
